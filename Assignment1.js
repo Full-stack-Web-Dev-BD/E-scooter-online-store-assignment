2bd65954ef07e27e8d3e406da4e25ce95cdb86e4
Alamin Hossen <alaminprogramerr@gmail.com>
2022-10-06 15:53:18 +0600

    added comment

--- Assignment1.js
+++ Assignment1.js
@@ -7,13 +7,16 @@ app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('public')); 
 
+// Serve Home page 
 app.get('/', (req, res) => {
   res.sendFile(`select.html`, { root: path.join(__dirname, './Files') });
 });
+// Serve all of product  by single request using req.params.variable
 app.get('/select/:variable', (req, res) => {
   res.sendFile(`${req.params.variable}.html`, { root: path.join(__dirname, './Files') });
 }) 
 
+// Handle non-existing file request error
 app.get("*", (req, res) => {
   res.send("File Not Found.")
 }) 
